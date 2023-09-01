5a604c3e2060225ce73ca10eb75163b3ef79d0ea
dyadyaJora <ja_massa97@mail.ru>
2023-09-01 23:11:32 +0400

    add students_and_examinations

--- main.py
+++ main.py
@@ -187,3 +187,12 @@ def find_managers(employee: pd.DataFrame) -> pd.DataFrame:
     result = result.merge(employee[['id', 'name']], left_on='managerId', right_on='id', how='inner')
     result = result[['name']]
     return result
+
+
+def students_and_examinations(students: pd.DataFrame, subjects: pd.DataFrame, examinations: pd.DataFrame) -> pd.DataFrame:
+    examinations = examinations.groupby(['student_id', 'subject_name']).agg(attended_exams=('subject_name', 'count')).reset_index()
+    students = students.merge(subjects, how='cross')
+    examinations = examinations.merge(students, on=['student_id', 'subject_name'],how='right')
+    examinations = examinations.fillna(0)
+    examinations = examinations.sort_values(['student_id', 'subject_name'])
+    return examinations[['student_id', 'student_name', 'subject_name', 'attended_exams']]
